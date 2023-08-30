0e6eda200cbec92222d44c25a96c4cc85358408f
AgathaKarenne <142948129+AgathaKarenne@users.noreply.github.com>
2023-08-30 11:08:58 -0300

    Add files via upload

---
+++ exercicio1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include <math.h>
+
+main(){
+
+
+    //Escreva um algoritmo que leia um número e imprima a raiz quadrada do número caso ele seja positivo ou igual a zero e o quadrado do número caso ele seja negativo.
+
+    double numero, raiz;
+
+    printf("Digite um numero: ");
+    scanf("%lf", &numero);
+
+    raiz = sqrt(numero);
+
+    printf("A raiz quadrada é %lf = %lf", numero, raiz);
+    scanf("%lf", &raiz);
+
+    if(numero == 0){
+
+        printf("A raiz quadrada é positiva");
+
+
+    }else if(numero != 0){
+
+        printf("A raiz quadrada é negativa");
+    }
+
+
+}
