42cd0ff97d11ea45c742b9cb623c5c4dde383694
AgathaKarenne <142948129+AgathaKarenne@users.noreply.github.com>
2023-09-05 10:32:25 -0300

    Update exercicio8.c

--- exercicio8.c
+++ exercicio8.c
@@ -5,7 +5,7 @@ main(){
 /*Leia a idade e o tempo de serviço de um trabalhador e escreva se ele pode ou não se  aposentar. As condições para aposentadoria são: • Ter pelo menos 65 anos;
  • Ou ter trabalhado pelo menos 30 anos;, • Ou ter pelo menos 60 anos e trabalhado pelo menos 25 anos. */
 
- int idade, anosDetrabalho, minimoDaIdade, tempoDeTrabalho;
+ int minimoDaIdade, anosDetrabalho, idade, tempoDeTrabalho;
 
  printf("Digite a idade do trabalhador: ");
  scanf("%d", &minimoDaIdade);
@@ -16,11 +16,15 @@ main(){
  printf("Digite a idade minima do trabalhador");
  scanf("%d", &minimoDaIdade);
 
- if(minimoDaIdade >= 65, anosDetrabalho >= 30, idade >= 60, tempoDeTrabalho >= 25){
+    if(minimoDaIdade > 65, anosDetrabalho > 30, idade > 60, tempoDeTrabalho > 25){
     printf("Pode se aposentar");
 
- }else if(minimoDaIdade <= 65, anosDetrabalho <= 30, idade <= 60, tempoDeTrabalho <= 25){
+ }else{
+
+     if(minimoDaIdade < 65, anosDetrabalho < 30, idade < 60, tempoDeTrabalho < 25){
     printf("Não pode se aposentar");
  }
+
+    }  
 
 }
